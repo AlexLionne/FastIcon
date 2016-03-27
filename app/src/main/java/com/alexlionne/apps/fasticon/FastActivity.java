62befbd93b0ee76de7ae733c559e5f177e62791a
AlexLionne <alextitou3@gmail.com>
2016-03-27 17:12:18 +0200

    Initial commit

---
+++ app/src/main/java/com/alexlionne/apps/fasticon/FastActivity.java
@@ -0,0 +1,123 @@
+package com.alexlionne.apps.fasticon;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebView;
+
+import com.mikepenz.community_material_typeface_library.CommunityMaterial;
+import com.mikepenz.iconics.IconicsDrawable;
+
+import java.io.File;
+import java.util.concurrent.ExecutionException;
+
+public class FastActivity extends AppCompatActivity {
+    private WebView webview;
+    private Bitmap bitmap;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fast);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        assert toolbar != null;
+        toolbar.setTitleTextColor(getResources().getColor(R.color.colorPrimaryDark));
+        getSupportActionBar().setElevation(0);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        webview = (WebView)findViewById(R.id.webView);
+
+        assert fab != null;
+        fab.setImageDrawable(new IconicsDrawable(this, CommunityMaterial.Icon.cmd_attachment).sizeDp(18).color(Color.WHITE));
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectIcon();
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+
+
+    public void selectIcon() {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        photoPickerIntent.setType("file/*");
+        startActivityForResult(photoPickerIntent, 1);
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == Activity.RESULT_OK) {
+                try {
+                    Uri selectedImage = data.getData();
+
+                    String filePath = getPath(selectedImage);
+                    String file_extn = filePath.substring(filePath.lastIndexOf(".") + 1);
+                    File file_bitmap = new File(filePath);
+
+
+                    if (file_extn.equals("html")) {
+
+
+                        if (Build.VERSION.SDK_INT >= 21) {
+                            Window window = getWindow();
+                            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+                            //window.setStatusBarColor(Utils.getLightPalettefromBitmap(bitmap));
+                            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                            //window.setNavigationBarColor(Utils.getLightPalettefromBitmap(bitmap));
+                        }
+
+                        webview.loadUrl("");
+
+                    } else {
+                        //NOT IN REQUIRED FORMAT
+                    }
+                } catch (Exception e) {
+
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+
+
+    public String getPath(Uri uri) {
+        String[] projection = {MediaStore.MediaColumns.DATA};
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        int column_index = cursor
+                .getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+        cursor.moveToFirst();
+
+        return cursor.getString(column_index);
+    }
+}
